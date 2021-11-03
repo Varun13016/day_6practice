a95d4c8ea4bd468d62133392ea7b6cf832920fbf
“Varun13016” <dmevarun@gmail.com>
2021-11-03 18:54:05 +0530

    day6

---
+++ Reversenum/Reversenum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reversenum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("enter the num");
+                int perNum = int.Parse(Console.ReadLine());
+                int rev = 0;
+                while (perNum > 0)
+                {
+                    int rem = perNum % 10;
+                    rev = (rev * 10) + rem;
+                    perNum = perNum / 10;
+                }
+                Console.WriteLine("reverse number is:" + rev);
+         }
+    }
+}
