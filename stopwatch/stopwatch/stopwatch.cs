4a7e2ca3b7bb85aaae515e202e7d58186387fa1e
“Varun13016” <dmevarun@gmail.com>
2021-11-09 19:17:02 +0530

    day6

---
+++ stopwatch/stopwatch/stopwatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace stopwatch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(10);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("elapse time:{0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+        }
+        
+    }
+}
